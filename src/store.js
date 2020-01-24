aee8f8fc480f559aff8e52fcfe4e1582ac41636d
System Administrator <conary@hotmail.es>
2020-01-24 10:01:55 -0600

    Redux setup

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : f => f;
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(thunk), reduxDevTools)
+);
+
+export default store;
