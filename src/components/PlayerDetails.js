823706c8e60ee5c5694265c5e414e4fd860c6b2b
System Administrator <conary@hotmail.es>
2020-01-28 16:13:58 -0600

    Modal Player

---
+++ src/components/PlayerDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+export default function PlayerDetails(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+PlayerDetails.propTypes = {
+  playersData: PropTypes.array,
+};
--- src/containers/Players.js
+++ src/containers/Players.js
@@ -1,14 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Modal from '@material-ui/core/Modal';
 import Grid from '@material-ui/core/Grid';
 
-
 export default function Players() {
+  const [openModal, setOpenModal] = useState(false);
   const [playersData, setPlayersData] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState({});
 
   useEffect(() => {
     getPlayersData();
   }, []);
+  const handleOpen = () => {
+    setOpenModal(true);
+  };
+  const handleClose = () => {
+    setOpenModal(false);
+  };
 
   const getPlayersData = () => {
     axios.get("/players").then(res => setPlayersData(res.data));
@@ -22,7 +30,7 @@ export default function Players() {
       return (<Grid key={index} item xs={4} sm={3} lg={2}>
         <div className="player-info">
           <div className="img-wrapper">
-            <img src={player.image} alt={`${player.name}`}/>
+            <img id={player.number} src={player.image} alt={`${player.name}`} onClick={onPlayerClick}/>
           </div>
           <span className="info-item">{player.position}</span>
           <span className="info-item">{player.name}</span>
@@ -31,12 +39,35 @@ export default function Players() {
     });
   };
 
+  const getSelectedPlayer = (playersData, currentPlayerId) => {
+    return playersData.filter(player => player.number === parseInt(currentPlayerId, 10))
+  };
+
+  const onPlayerClick = (e) => {
+    const currentPlayer = getSelectedPlayer(consolidatedData, e.target.id);
+    setSelectedPlayer(currentPlayer[0]);
+    handleOpen();
+  }
+
   return (
     <div id="players">
+      <button type="button" onClick={handleOpen}>
+        Open Modal
+      </button>
       <h1>Players</h1>
       <Grid container spacing={0} direction="row" justify="center" alignItems="center">
         {generatePlayerItemList(consolidatedData)}
       </Grid>
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={openModal}
+        onClose={handleClose}
+      >
+        <div className="player-modal">
+          <h1>{selectedPlayer.name}</h1>
+        </div>
+      </Modal>
     </div>
   )
 }
